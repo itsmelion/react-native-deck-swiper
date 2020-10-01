80b25d6997e0ce021512bea24c285ea17cb5a197
Christhopher Leão <christhopherleao@icloud.com>
2020-10-01 15:55:48 +0100

    tweak import styled/native

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 import { Animated } from 'react-native';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 export const Card = styled(Animated.View)`
   position: absolute;
